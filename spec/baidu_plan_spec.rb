08d550a43a7ec6010be708221e536d44050165f2
charles <charles.liu@corp.elong.com>
2014-09-26 20:04:03 +0800

    move plan specs into account specs

--- spec/baidu_plan_spec.rb
+++ spec/baidu_plan_spec.rb
@@ -6,32 +6,6 @@ describe ::PPC::Baidu::Plan do
             token:$baidu_token
   )}
 
-
-  it "can update one plan" do
-    random_plan_id = subject.ids.first
-    new_name = "测试计划#{Time.now.to_i}"
-    response = subject.update_by_id(random_plan_id,{campaignName: new_name})
-    expect(response.class).to be Array
-    expect(response.first.class).to be Hash
-    expect(response.first['campaignName']).to eq new_name
-  end
-
-  it "can update two plans" do
-    random_plan_id1 = subject.ids[0]
-    random_plan_id2 = subject.ids[1]
-
-    new_name1 = "测试计划1_#{Time.now.to_i}"
-    new_name2 = "测试计划2_#{Time.now.to_i}"
-
-    plans = []
-    plans << {campaignId: random_plan_id1,campaignName: new_name1}
-    plans << {campaignId: random_plan_id2,campaignName: new_name2}
-    response = subject.update(plans)
-    expect(response.class).to be Array
-    expect(response.first.class).to be Hash
-    expect(response[0]['campaignName']).to eq new_name1
-    expect(response[1]['campaignName']).to eq new_name2
-  end
   it "can get all plans" do
     response = subject.plans
     expect(response.class).to be Array
@@ -39,34 +13,13 @@ describe ::PPC::Baidu::Plan do
     expect(response.first['campaignId']).to be > 0
   end
 
-  it "can add a plan" do
-    response = subject.add({name:"测试计划#{Time.now.to_i}"})
-    expect(response).to have_key 'campaignId'
-    expect(response['campaignId']).to be > 0
-  end
 
-  it "can add two plans" do
-    plan1 = {name:"测试计划1#{Time.now.to_i}"}
-    plan2 = {name:"测试计划2#{Time.now.to_i}"}
-
-    response = subject.add([plan1,plan2])
-    expect(response.class).to be Array
-    expect(response.first['campaignId']).to be > 0
-    expect(response[1]['campaignId']).to be > 0
-  end
 
   it "can get all plan ids" do
     response = subject.ids()
     expect(response.class).to be Array
   end
 
-  it "can get plans by one id" do
-    id = subject.ids().first
-    response = subject.get(id)
-    expect(response.class).to be Hash
-    expect(response['campaignId']).to be id
-    expect(response).to have_key 'campaignName'
-  end
 
   it "can delete one plan by the id" do
     ids = subject.ids()
