c1515cfd0c3947763610f8b6958ce7a642fae8d1
ChienliMa <maqianlie@gmail.com>
2014-12-05 15:22:52 +0800

    Change default value in operation::report; Add debug print to operation::report

--- lib/ppc/operation/report.rb
+++ lib/ppc/operation/report.rb
@@ -3,49 +3,40 @@ module PPC
     module Report
 
       def query_report( param = nil )
-        if not param
-          param = {}
-          param[:type]   = 'pair'
-          param[:fields] =  %w(click impression)
-          param[:level]  = 'pair'
-          param[:range]  = 'account'
-          param[:unit]   = 'day'
-        end
+        param[:type]   ||= 'pair'
+        param[:fields] ||=  %w(click impression)
+        param[:level]  ||= 'pair'
+        param[:range]  ||= 'account'
+        param[:unit]   ||= 'day'
         download_report( param )
       end
 
       def creative_report( param = nil )
-        if not param
-          param = {}
-          param[:type]   = 'creative'
-          param[:fields] =  %w(impression click cpc cost ctr cpm position conversion)
-          param[:level]  = 'creative'
-          param[:range]  = 'creative'
-          param[:unit]   = 'day'
-        end
+        param[:type]   ||= 'creative'
+        param[:fields] ||=  %w(impression click cpc cost ctr cpm position conversion)
+        param[:level]  ||= 'creative'
+        param[:range]  ||= 'creative'
+        param[:unit]   ||= 'day'
         download_report( param )
       end
 
       def keyword_report( param = nil )
-        if not param
-          param = {}
-          param[:type]   = 'keyword'
-          param[:fields] =  %w(impression click cpc cost ctr cpm position conversion)
-          param[:level]  = 'keywordid'
-          param[:range]  = 'keywordid'
-          param[:unit]   = 'day'
-        end
+        param[:type]   ||= 'keyword'
+        param[:fields] ||=  %w(impression click cpc cost ctr cpm position conversion)
+        param[:level]  ||= 'keywordid'
+        param[:range]  ||= 'keywordid'
+        param[:unit]   ||= 'day'
         download_report( param )
       end
  
-      def download_report( param )
+      def download_report( param, debug = false )
         id = call('report').get_id( @auth, param )[:result]
-        p "get report id:" + id.to_s
+        p "Got report id:" + id.to_s if debug 
 
         loop do
           sleep 3 
           break if call('report').get_state( @auth, id )[:result] == 'Finished'
-          p "Report is not generated, waiting..."
+          p "Report is not generated, waiting..." if debug 
         end
 
         url = call('report').get_url( @auth, id )[:result]
