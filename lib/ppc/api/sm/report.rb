cc0fc344aa59c073a35628fc3df6cc4bc8e35da7
范捷琦 <jieqi.fan@corp.elong.com>
2015-04-13 11:50:40 +0800

    change the sm/report.rb

--- lib/ppc/api/sm/report.rb
+++ lib/ppc/api/sm/report.rb
@@ -4,16 +4,16 @@ module PPC
     class Sm
       class Report< Sm
         Service = 'Report'
-      
+
         # 需要用到的映射集合
-        Type_map = { 'account' => 2, 'plan'=> 10, 'group'=> 11, 
+        Type_map = { 'account' => 2, 'plan'=> 10, 'group'=> 11,
                       'keyword'=> 14, 'creative'=> 12,
-                       'region'=> 3, 'query'=>6, 'trail' => 21, "tel" => 22, 
+                       'region'=> 3, 'query'=>6, 'trail' => 21, "tel" => 22,
                        "app" => 23, "none" => 24}
 
-        Level_map = {  'account' => 2, 'plan' => 3, 'group' => 5, 
+        Level_map = {  'account' => 2, 'plan' => 3, 'group' => 5,
                         'creative'=> 7, 'keywordid' => 11, 'trail' => 21, "tel" => 22,
-                         "app" => 23}
+                         "app" => 23, 'query_creative' => 12}
 
         Device_map = { 'all' => 0, 'pc' => 1, 'mobile' => 2 }
 
@@ -24,26 +24,30 @@ module PPC
         ########################
         def self.get_id( auth, params  )
           body = make_reportrequest( params )
-          response = request( auth, Service, 'report/getReport' ,body) 
-          process( response, 'reportId' ){ |x| x }
+          response = request( auth, 'report' , 'getReport' ,body)
+          process( response, 'taskId' ){ |x| x }
         end
 
         def self.get_state( auth, id )
           '''
           input id should be string
           '''
-          status = {1=>'Waiting' ,2=>'Opearting' ,3=>'Finished'}
-          body = { reportId:  id }
-          response = request( auth, Service, 'task/getTaskState' ,body)
-          process( response, 'isGenerated' ){ |x| status[x] }
+          body = { taskId:  id }
+          response = request( auth, 'task' , 'getTaskState' ,body)
+          process( response, 'status' ){ |x| x }
         end
 
-        def self.get_url( auth,  id  )
-          body = { reportId:  id }
-          response = request( auth, Service, 'file/download' ,body)
-          process( response, 'reportFilePath' ){ |x| x }       
+        def self.get_fileId( auth,  id  )
+          body = { taskId:  id }
+          response = request( auth, 'task' , 'getTaskState' ,body)
+          process( response, 'fileId' ){ |x| x }
         end
-        
+
+        def self.get_file( auth,  id  )
+          body = { fileId:  id }
+          response = request( auth, 'file' , 'download' ,body)
+        end
+
         private
         def self.make_reportrequest( param )
           """
@@ -57,32 +61,29 @@ module PPC
           """
           requesttype = {}
           requesttype[:performanceData]   =     param[:fields]  || %w(click impression cost cpc ctr)
-          requesttype[:reportType]        =     Type_map[ param[:type] ]      if  param[:type]
-          requesttype[:levelOfDetails]    =     Level_map[  param[:level] ]   if param[:level]
+          requesttype[:reportType]        =     Type_map[ param[:type] ]      if param[:type]
+          requesttype[:levelOfDetails]    =     Level_map[ param[:level] ]    if param[:level]
           requesttype[:statRange]         =     Level_map[ param[:range] ]    if param[:range]
           requesttype[:unitOfTime]        =     Unit_map[ param[:unit] ]      if param[:unit]
           requesttype[:device]            =     Device_map[ param[:device] ]  if param[:device]
           requesttype[:idOnly]            =     param[:id_only] || false
           requesttype[:startDate]         =     parse_date( param[:startDate] )
           requesttype[:endDate]           =     parse_date( param[:endDate] )
+          requesttype[:statIds]           =     param[:statIds] if param[:type] == "keyword" && param[:range] != "account"
           return requesttype
         end
 
-        private 
+        private
         def self.parse_date( date )
           """
           Cast string to time:
           'YYYYMMDD' => Time
           """
           if date
-            y = date[0..3]
-            m = date[4..5]
-            d = date[6..7]
-            date = Time.new( y, m, d )
+            date = Date.parse(date)
           else
-            date = (Time.now - 24*3600)
+            date = Date.today
           end
-          date
         end
 
         #################################
@@ -91,7 +92,6 @@ module PPC
         def self.query_report( auth, param = {}, debug = false )
           param[:type]   ||= 'query'
           param[:fields] ||=  %w(click impression)
-          param[:level]  ||= 'pair'
           param[:range]  ||= 'account'
           param[:unit]   ||= 'day'
           download_report( auth, param, debug )
@@ -99,42 +99,32 @@ module PPC
 
         def self.creative_report( auth, param = {}, debug = false )
           param[:type]   ||= 'creative'
-          param[:fields] ||=  %w(impression click cpc cost ctr cpm position conversion)
-          param[:level]  ||= 'creative'
-          param[:range]  ||= 'creative'
+          param[:fields] ||=  %w(click impression)
+          param[:range]  ||= 'account'
           param[:unit]   ||= 'day'
           download_report( auth, param, debug )
         end
 
         def self.keyword_report( auth, param = {}, debug = false )
           param[:type]   ||= 'keyword'
-          param[:fields] ||=  %w(impression click cpc cost ctr cpm position conversion)
-          param[:level]  ||= 'keywordid'
-          param[:range]  ||= 'keywordid'
+          param[:fields] ||=  %w(click impression cost cpc ctr)
+          param[:range]  ||= 'account'
           param[:unit]   ||= 'day'
           download_report( auth, param, debug )
         end
-   
+
         def self.download_report( auth, param, debug = false )
-          p param
           response = get_id( auth, param )
-          warn "-"*50
-          warn response.code
-          warn response['location']
-          warn "#{response.body.map{|k,v| [k,v]}}"
-          warn "-"*50
-          return response
           if response[:succ]
             id = response[:result]
-            p "Got report id:" + id.to_s if debug 
+            p "Got report id:" + id.to_s if debug
             loop do
-              sleep 2 
-              break if get_state( auth, id )[:result] == 'Finished'
-              p "Report is not generated, waiting..." if debug 
+              sleep 2
+              break if get_state( auth, id )[:result] == 'FINISHED'
+              p "Report is not generated, waiting..." if debug
             end
-
-            url = get_url( auth, id )[:result]
-            return open(url).read.force_encoding('gb18030').encode('utf-8')
+            fileId = get_fileId(auth, id)[:result]
+            return get_file(auth, fileId).force_encoding("GB2312").encode("UTF-8")
           else
             raise response[:failure][0]["message"]
           end
