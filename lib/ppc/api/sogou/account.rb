213bdbe2e421832209fa24d89a66741a6a30daee
范捷琦 <jieqi.fan@corp.elong.com>
2016-05-20 18:39:09 +0800

    simplify the code

--- lib/ppc/api/sogou/account.rb
+++ lib/ppc/api/sogou/account.rb
@@ -19,8 +19,8 @@ module PPC
         }
         @map = AccountType
 
-        def self.info(auth)
-          response = request(auth,Service,'getAccountInfo'  )
+        def self.info( auth )
+          response = request(auth,Service,'getAccountInfo' )
           process( response, 'accountInfoType' ){ |x|reverse_type(x)[0] }
         end
 
@@ -31,8 +31,7 @@ module PPC
           @return : account info_type
           """
           # for account service, there is not bulk operation
-          infoType = make_type( param )[0]
-          body = { accountInfoType: infoType }
+          body = { accountInfoType: make_type( param )[0] }
           response = request(auth,Service,'updateAccountInfo', body)
           process( response, 'accountInfoType' ){ |x|reverse_type(x)[0] }
         end
