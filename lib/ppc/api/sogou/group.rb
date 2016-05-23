19e4ee6825eab6163011caf924dd440bbaefff1a
范捷琦 <jieqi.fan@corp.elong.com>
2016-05-23 14:34:53 +0800

    change the sogou:group

--- lib/ppc/api/sogou/group.rb
+++ lib/ppc/api/sogou/group.rb
@@ -19,107 +19,55 @@ module PPC
           "id" =>         :cpc_grp_id,
           "plan_name" =>  :cpc_plan_name,
           "name" =>       :cpc_grp_name,
+          "price" =>      :max_price,
         }
         @map = GroupType
 
-        def self.ids( auth )
-          """
-          @return : Array of cpcPlanGrpIdTypes
-          """
-          response = request( auth, Service , "getAllCpcGrpId" )
-          #此处返回值与key与开发文档不同
-          process( response, 'cpcPlanGrpIds' ){ |x| make_planGroupIds( x ) }
+        def self.info( auth, ids)
+          response = request(auth, Service, "getCpcGrpByCpcGrpId", {cpcGrpIds: ids} )
+          process( response, 'cpc_grp_types' ){ |x| reverse_type(x)[0] }
+        end
+
+        def self.all( auth, plan_ids )
+          response = request( auth, Service ,"getCpcGrpByCpcPlanId",  {cpcPlanIds: plan_ids} )
+          process( response, 'cpc_plan_grps' ){ |x| reverse_type( x[:cpc_grp_types] ) }
+        end
+
+        def self.ids( auth, plan_ids )
+          response = request( auth, Service ,"getCpcGrpIdByCpcPlanId",  {cpcPlanIds: plan_ids} )
+          process( response, 'cpcPlanGrpIds' ){ |x| reverse_type( x ) }
         end
 
         def self.get( auth, ids )
-          ids = [ ids ] unless ids.is_a? Array
-          body = { cpcGrpIds: ids }
-          response = request(auth, Service, "getCpcGrpByCpcGrpId",body )
-          process( response, 'cpcGrpTypes' ){ |x| reverse_type(x) }
+          response = request(auth, Service, "getCpcGrpByCpcGrpId", {cpcGrpIds: ids} )
+          process( response, 'cpc_grp_types' ){ |x| reverse_type(x) }
         end
 
         def self.add( auth, groups )
-          """
-          @ input : one or list of AdgroupType
-          @ output : list of AdgroupType
-          """
-          cpcGrpTypes = make_type( groups )
-
-          body = {cpcGrpTypes:  cpcGrpTypes }
-          
+          body = {cpcGrpTypes: make_type( groups ) }
           response = request( auth, Service, "addCpcGrp", body  )
-          process( response, 'cpcGrpTypes' ){ |x| reverse_type(x) }
+          process( response, 'cpc_grp_types' ){ |x| reverse_type(x) }
         end
 
         def self.update( auth, groups )
-          """
-          @ input : one or list of AdgroupType
-          @ output : list of AdgroupType
-          """
-          cpcGrpTypes = make_type( groups )
-          body = {cpcGrpTypes: cpcGrpTypes}
-          
+          body = {cpcGrpTypes: make_type( groups )}
           response = request( auth, Service, "updateCpcGrp",body )
-          process( response, 'cpcGrpTypes' ){ |x| reverse_type(x) }
+          process( response, 'cpc_grp_types' ){ |x| reverse_type(x) }
         end
 
         def self.delete( auth, ids )
-          """
-          delete group body has no message
-          """
-          ids = [ ids ] unless ids.is_a? Array
-          body = { cpcGrpIds: ids }
-          response = request( auth, Service,"deleteCpcGrp", body )
-          process( response, '' ){ |x|  x  }
+          response = request( auth, Service,"deleteCpcGrp", {cpcGrpIds: ids} )
+          process( response, '' ){ |x| x }
         end
 
-        def self.search_by_plan_id( auth, ids )
-          ids = [ ids ] unless ids.is_a? Array
-          body = { cpcPlanIds: ids }
-          response = request( auth, Service ,"getCpcGrpByCpcPlanId",  body )
-          # 此处key与开发文档不同
-          process( response, 'cpcPlanGrps' ){ |x| make_planGroups( x ) }
+        def self.enable( auth, ids )
+          groups = ids.map{|id| {id: id, pause: false} }
+          self.update( auth, groups )
         end
 
-        def self.search_id_by_plan_id( auth, ids )
-          ids = [ ids ] unless ids.is_a? Array
-          body = { cpcPlanIds: ids }
-          response = request( auth, Service ,"getCpcGrpIdByCpcPlanId",  body )
-          process( response, 'cpcPlanGrpIds' ){ |x| make_planGroupIds( x ) }
-        end
-
-        private
-        def self.make_planGroupIds( cpcPlanGrpIdTypes )
-          """
-          Transfer Sogou API to PPC API
-          Item: cpc_plan_group_ids
-          """
-          cpcPlanGrpIdTypes = [cpcPlanGrpIdTypes] unless cpcPlanGrpIdTypes.is_a? Array
-          planGroupIds = []
-          cpcPlanGrpIdTypes.each do |cpcPlanGrpIdType|
-            planGroupId = {}
-            planGroupId[:plan_id] = cpcPlanGrpIdType[:cpc_plan_id]
-            planGroupId[:group_ids] = cpcPlanGrpIdType[:cpc_grp_ids]
-            planGroupIds << planGroupId
-          end
-          return planGroupIds
-        end
-
-        private
-        def self.make_planGroups( cpcPlanGrpTypes )
-          """
-          Transfer Sogou API to PPC API
-          """
-          # 多加一行change成array
-          cpcPlanGrpTypes = [cpcPlanGrpTypes] unless cpcPlanGrpTypes.is_a? Array
-          planGroups = []
-          cpcPlanGrpTypes.each do |cpcPlanGrpType|
-            planGroup = {}
-            planGroup[:plan_id] = cpcPlanGrpType[:cpc_plan_id]
-            planGroup[:groups] = reverse_type( cpcPlanGrpType[:cpc_grp_types] )
-            planGroups << planGroup
-          end
-          return planGroups
+        def self.pause( auth, ids )
+          groups = ids.map{|id| {id: id, pause: true} }
+          self.update( auth, groups )
         end
 
       end # class group
