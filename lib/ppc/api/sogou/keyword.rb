039a7cd61faa48b3d52b1a323bb90bfeac7642ce
范捷琦 <jieqi.fan@corp.elong.com>
2016-05-23 16:04:52 +0800

    change sogou:keyword

--- lib/ppc/api/sogou/keyword.rb
+++ lib/ppc/api/sogou/keyword.rb
@@ -41,106 +41,62 @@ module PPC
           status: :status,
         }
 
-        # 后面改成info方法
+        def self.info( auth, ids )
+          response = request( auth, Service, 'getCpcByCpcId', {cpcIds: ids} )
+          process(response, 'cpcTypes'){|x| reverse_type( x )[0] }
+        end
+
+        def self.all( auth, group_ids )
+          response = request( auth, Service, "getCpcByCpcGrpId", {cpcGrpIds: group_ids} )
+          process(response, 'cpcGrpCpcs'){|x| reverse_type( x[:cpc_types] ) }
+        end
+
+        def self.ids( auth, group_ids )
+          response = request( auth, Service, "getCpcIdByCpcGrpId", {cpcGrpIds: group_ids} )
+          process(response, 'cpcGrpCpcIds'){|x| reverse_type( x ) }
+        end
+
         def self.get( auth, ids )
-          '''
-          getCpcByCpcId
-          '''
-          ids = [ ids ] unless ids.is_a? Array
-          body = { cpcIds: ids}
-          response = request( auth, Service, 'getCpcByCpcId', body )
+          response = request( auth, Service, 'getCpcByCpcId', {cpcIds: ids} )
           process(response, 'cpcTypes'){|x| reverse_type( x ) }
         end
 
         def self.add( auth, keywords )
-          '''
-          '''
-          cpcTypes = make_type( keywords ) 
-          body = { cpcTypes: cpcTypes }
+          body = { cpcTypes: make_type( keywords )  }
           response = request( auth, Service, "addCpc", body )
           process(response, 'cpcTypes'){|x| reverse_type(x)  }
         end
 
         def self.update( auth, keywords )
-          '''
-          '''
-          cpcTypes = make_type( keywords ) 
-          body = { cpcTypes: cpcTypes }
+          body = { cpcTypes: make_type( keywords )  }
           response = request( auth, Service, "updateCpc", body )
           process(response, 'cpcTypes'){|x| reverse_type(x)  }
         end
 
         def self.delete( auth, ids )
-          """
-          """
-          ids = [ ids ] unless ids.is_a? Array
-          body = { cpcIds: ids}
-          response = request( auth, Service, 'deleteCpc', body )
+          response = request( auth, Service, 'deleteCpc', {cpcIds: ids} )
           process(response, ''){|x| x }
         end
 
-        def self.all( auth, group_ids )
-          group_ids = [ group_ids ] unless group_ids.is_a? Array
-          body = { cpcGrpIds: group_ids }
-          response = request( auth, Service, "getCpcByCpcGrpId", body )
-          process(response, 'cpcGrpCpcs'){|x| make_groupKeywords( x ) }
+        def self.enable( auth, ids )
+          keywords = ids.map{|id| {id: id, pause: false} }
+          self.update( auth, keywords )
         end
 
-        def self.ids( auth, group_ids )
-          group_ids = [ group_ids ] unless group_ids.is_a? Array
-          body = { cpcGrpIds: group_ids }
-          response = request( auth, Service, "getCpcIdByCpcGrpId", body )
-          process(response, 'cpcGrpCpcIds'){|x| make_groupKeywordIds( x ) }
+        def self.pause( auth, ids )
+          keywords = ids.map{|id| {id: id, pause: true} }
+          self.update( auth, keywords )
         end
 
         # sogou的keyword服务不提供质量度和状态，从getInfo方法中查询
         def self.status( auth, ids )
-          '''
-          Return [ { id: id, status: status} ... ]
-          '''
-          ids = [ ids ] unless ids.is_a? Array
-          body = { cpcIds: ids}
-          response = request( auth, Service, 'getCpcByCpcId', body )
-          process(response, 'cpcTypes'){  |x|  reverse_type(x, @status_map) }
+          response = request( auth, Service, 'getCpcByCpcId', {cpcIds: ids} )
+          process(response, 'cpcTypes'){|x|  reverse_type(x, @status_map) }
         end
 
         def self.quality( auth ,ids )
-          '''
-          Return [ { id: id, quality: quality} ... ]
-          '''
-          ids = [ ids ] unless ids.is_a? Array
-          body = { cpcIds: ids}
-          response = request( auth, Service, 'getCpcByCpcId', { cpcIds: ids} )
-          process(response, 'cpcTypes'){  |x| reverse_type(x, @quality_map) }
-        end
-
-        private
-        def self.make_groupKeywordIds( cpcGrpCpcIdTypes )
-          """
-          Transfer Sogou API to PPC API
-          """
-          cpcGrpCpcIdTypes = [cpcGrpCpcIdTypes] unless cpcGrpCpcIdTypes.is_a? Array
-          group_keyword_ids = []
-          cpcGrpCpcIdTypes.each do |cpcGrpCpcIdType|
-            group_keyword_id = { }
-            group_keyword_id[:group_id] = cpcGrpCpcIdType[:cpc_grp_id]
-            group_keyword_id[:keyword_ids] = cpcGrpCpcIdType[:cpc_ids]
-            group_keyword_ids << group_keyword_id
-          end
-          return group_keyword_ids
-        end
-
-        private
-        def self.make_groupKeywords( cpcGrpCpcTypes )
-          cpcGrpCpcTypes = [cpcGrpCpcTypes] unless cpcGrpCpcTypes.is_a? Array 
-          group_keywords = []
-          cpcGrpCpcTypes.each do |cpcGrpCpcType|
-            group_keyword = {}
-            group_keyword[:group_id] = cpcGrpCpcType[:cpc_grp_id]
-            group_keyword[:keywords] = reverse_type( cpcGrpCpcType[:cpc_types] )
-            group_keywords << group_keyword
-          end
-          return group_keywords
+          response = request( auth, Service, 'getCpcByCpcId', {cpcIds: ids} )
+          process(response, 'cpcTypes'){|x| reverse_type(x, @quality_map) }
         end
 
       end # keyword
