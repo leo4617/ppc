0e4a6b053524d61ac0aae0619e280ef623b68232
范捷琦 <jieqi.fan@corp.elong.com>
2016-05-25 17:29:18 +0800

    change sm:creative

--- lib/ppc/api/sm/creative.rb
+++ lib/ppc/api/sm/creative.rb
@@ -14,8 +14,25 @@ module PPC
           mobile_destination: :destinationUrl,
           mobile_display:     :displayUrl,
           status:             :status,
+          creative_ids:       :creativeIds,
+          creatives:          :creativeTypes,
         }
         @map = CreativeType
+
+        def self.info(auth, ids)
+          response = request(auth, Service, 'getCreativeByCreativeId', {creativeIds: ids})
+          process(response, 'creativeTypes'){|x| reverse_type(x)}
+        end
+
+        def self.all(auth, ids)
+          response = request(auth, Service, 'getCreativeByAdgroupId', {adgroupIds: ids})
+          process(response, 'groupCreatives'){|x| reverse_type(x)}
+        end
+
+        def self.ids(auth, ids)
+          response = request(auth, Service, 'getCreativeIdByAdgroupId', {adgroupIds: ids})
+          process(response, 'groupCreativeIds'){|x| reverse_type(x)}
+        end
 
         def self.add(auth, creatives)
           body = {creativeTypes: make_type(creatives)}
@@ -24,9 +41,7 @@ module PPC
         end
 
         def self.get(auth, ids)
-          ids = [ids] unless ids.is_a? Array
-          body = {creativeIds: ids}
-          response = request(auth, Service, 'getCreativeByCreativeId', body)
+          response = request(auth, Service, 'getCreativeByCreativeId', {creativeIds: ids})
           process(response, 'creativeTypes'){|x| reverse_type(x)}
         end
 
@@ -37,48 +52,18 @@ module PPC
         end
 
         def self.delete(auth, ids)
-          ids = [ids] unless ids.is_a? Array
-          body = {creativeIds: ids}
-          response = request(auth, Service, 'deleteCreative', body, 'delete')
+          response = request(auth, Service, 'deleteCreative', {creativeIds: ids}, 'delete')
           process(response, 'result'){|x| x}
         end
 
-        def self.ids(auth, ids)
-          ids = [ids] unless ids.is_a? Array
-          body = {adgroupIds: ids}
-          response = request(auth, Service, 'getCreativeIdByAdgroupId', body)
-          process(response, 'groupCreativeIds'){|x| make_groupCreativeIds(x)}
+        def self.enable( auth, ids )
+          creatives = ids.map{|id| {id: id, pause: false} }
+          self.update( auth, creatives )
         end
 
-        def self.search_by_group_id(auth, ids)
-          ids = [ids] unless ids.is_a? Array
-          body = {adgroupIds: ids}
-          response = request(auth, Service, 'getCreativeByAdgroupId', body)
-          process(response, 'groupCreatives'){|x| make_groupCreatives(x)}
-        end
-
-        private
-        def self.make_groupCreativeIds( groupCreativeIds )
-          group_creative_ids = []
-          groupCreativeIds.each do |groupCreativeId|
-            group_creative_id = { }
-            group_creative_id[:group_id] = groupCreativeId['adgroupId']
-            group_creative_id[:creative_ids] = groupCreativeId['creativeIds']
-            group_creative_ids << group_creative_id
-          end
-          return group_creative_ids
-        end
-
-        private
-        def self.make_groupCreatives( groupCreatives )
-          group_creatives = []
-          groupCreatives.each do |groupCreative |
-            group_creative = {}
-            group_creative[:group_id] = groupCreative['adgroupId']
-            group_creative[:creatives] = reverse_type( groupCreative['creativeTypes'] )
-            group_creatives << group_creative
-          end
-          return group_creatives
+        def self.pause( auth, ids )
+          creatives = ids.map{|id| {id: id, pause: true} }
+          self.update( auth, creatives )
         end
 
       end
