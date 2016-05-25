b1d941b7505d7e3a495739bcd0e9868c5e2456a4
范捷琦 <jieqi.fan@corp.elong.com>
2016-05-25 17:13:00 +0800

    change sm:keyword

--- lib/ppc/api/sm/keyword.rb
+++ lib/ppc/api/sm/keyword.rb
@@ -16,73 +16,56 @@ module PPC
           match_type:         :matchType,
           status:             :status,
           pause:              :pause,
+          keyword_ids:        :keywordIds,
+          keywords:           :keywordTypes,
         }
         @map = KeywordType
 
-        def self.get(auth, ids)
-          ids = [ids] unless ids.is_a? Array
-          body = {keywordIds: ids}
-          response = request(auth, Service, 'getKeywordByKeywordId', body)
-          return process(response, 'keywordTypes'){|x| reverse_type(x)}
-        end
-
-        def self.add(auth, keywords)
-          keyword_types = make_type(keywords) 
-          body = {keywordTypes: keyword_types}
-          response = request(auth, Service, "addKeyword", body)
-          return process(response, 'keywordTypes'){|x| reverse_type(x)}
-        end
-
-        def self.update(auth, keywords)
-          keyword_types = make_type(keywords) 
-          body = {keywordTypes: keyword_types}
-          response = request(auth, Service, "updateKeyword", body)
-          return process(response, 'keywordTypes'){|x| reverse_type(x)}
-        end
-
-        def self.delete(auth, ids)
-          ids = [ids] unless ids.is_a? Array
-          body = {keywordIds: ids}
-          response = request(auth, Service, 'deleteKeyword', body, 'delete')
-          return process(response, 'result'){|x| x }
+        def self.info(auth, ids)
+          response = request(auth, Service, 'getKeywordByKeywordId', {keywordIds: ids})
+          process(response, 'keywordTypes'){|x| reverse_type(x)[0] }
         end
 
         def self.all(auth, group_ids)
-          group_ids = [group_ids] unless group_ids.is_a? Array
-          body = {adgroupIds: group_ids}
-          response = request(auth, Service, "getKeywordByAdgroupId", body)
-          return process(response, 'groupKeywords'){|x| make_groupKeywords(x)}
+          response = request(auth, Service, "getKeywordByAdgroupId", {adgroupIds: group_ids} )
+          process(response, 'groupKeywords'){|x| reverse_type(x)}
         end
 
         def self.ids(auth, group_ids)
-          group_ids = [group_ids] unless group_ids.is_a? Array
-          body = {adgroupIds: group_ids}
-          response = request(auth, Service, "getKeywordIdByAdgroupId", body)
-          return process(response, 'groupKeywordIds'){|x| make_groupKeywordIds(x)}
+          response = request(auth, Service, "getKeywordIdByAdgroupId", {adgroupIds: group_ids} )
+          process(response, 'groupKeywordIds'){|x| reverse_type(x)}
         end
 
-        private
-        def self.make_groupKeywordIds( groupKeywordIds )
-          group_keyword_ids = []
-          groupKeywordIds.each do |groupKeywordId|
-            group_keyword_id = { }
-            group_keyword_id[:group_id] = groupKeywordId['adgroupId']
-            group_keyword_id[:keyword_ids] = groupKeywordId['keywordIds']
-            group_keyword_ids << group_keyword_id
-          end
-          return group_keyword_ids
+        def self.get(auth, ids)
+          response = request(auth, Service, 'getKeywordByKeywordId', {keywordIds: ids})
+          process(response, 'keywordTypes'){|x| reverse_type(x)}
         end
 
-        private
-        def self.make_groupKeywords( groupKeywords )
-          group_keywords = []
-          groupKeywords.each do |groupKeyword|
-            group_keyword = {}
-            group_keyword[:group_id] = groupKeyword['adgroupId']
-            group_keyword[:keywords] = reverse_type( groupKeyword['keywordTypes'] )
-            group_keywords << group_keyword
-          end
-          return group_keywords
+        def self.add(auth, keywords)
+          body = {keywordTypes: make_type(keywords) }
+          response = request(auth, Service, "addKeyword", body)
+          process(response, 'keywordTypes'){|x| reverse_type(x)}
+        end
+
+        def self.update(auth, keywords)
+          body = {keywordTypes: make_type(keywords) }
+          response = request(auth, Service, "updateKeyword", body)
+          process(response, 'keywordTypes'){|x| reverse_type(x)}
+        end
+
+        def self.delete(auth, ids)
+          response = request(auth, Service, 'deleteKeyword', {keywordIds: ids}, 'delete')
+          process(response, 'result'){|x| x }
+        end
+
+        def self.enable( auth, ids )
+          keywords = ids.map{|id| {id: id, pause: false} }
+          self.update( auth, keywords )
+        end
+
+        def self.pause( auth, ids )
+          keywords = ids.map{|id| {id: id, pause: true} }
+          self.update( auth, keywords )
         end
 
       end # keyword
