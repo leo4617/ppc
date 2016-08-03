82ae38b0f66743cd1941687d6da9fb853e77a206
范捷琦 <jieqi.fan@corp.elong.com>
2016-08-03 16:56:02 +0800

    remove unused code

--- lib/ppc/api/qihu.rb
+++ lib/ppc/api/qihu.rb
@@ -32,20 +32,6 @@ module PPC
         "format=json&" + params.map{|k,v| "#{k.to_s}=#{v.is_a?(Array) ? v.to_json : v}"}.join('&')
       end
 
-#      def self.request( auth, service, method, params = {} )
-#        p 123
-#        url = "https://api.e.360.cn/2.0/#{service}/#{method}"
-#        # 日后考虑将httpparty用Net/http代替
-#        response = HTTParty.post(url, 
-#                body: params,
-#                headers: {'apiKey' => auth[:api_key], 
-#                                'accessToken' => auth[:token], 
-#                                'serveToken' => Time.now.to_i.to_s  }
-#              )
-#        p response 
-#        response.parsed_response
-#      end
-
       def self.process( response, key, failure_key = '', &func )
         result = {}
         result[:succ]     = response['failures'].nil? || response['failures'].size.zero?
